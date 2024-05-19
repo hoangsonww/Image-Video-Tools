e36c388d3f5ad458b599b3cfad9e7ac8109746dd
Son Nguyen <hoangson091104@gmail.com>
2024-05-19 19:19:09 +0700

    Update: Enhanced App Features

---
+++ src/js/about.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    if (localStorage.getItem('darkMode') === 'enabled') {
+        document.body.classList.add('dark-mode');
+    }
+});
+
+function toggleDarkMode() {
+    const body = document.body;
+    body.classList.toggle('dark-mode');
+
+    if (body.classList.contains('dark-mode')) {
+        localStorage.setItem('darkMode', 'enabled');
+    }
+    else {
+        localStorage.setItem('darkMode', 'disabled');
+    }
+}
