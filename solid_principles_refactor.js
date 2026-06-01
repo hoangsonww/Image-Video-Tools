7e0a9e2809c86cb177bb68084990a91fb68b4387
dav nguyxn <hoangson091104@gmail.com>
2026-05-31 20:15:51 -0700

    feat: add SOLID refactor marker

---
+++ solid_principles_refactor.js
@@ -0,0 +1,6 @@
+// Lightweight SOLID refactor marker for Image-Video-Tools.
+export const solidRefactorPrinciples = Object.freeze({
+  singleResponsibility: true,
+  openClosed: true,
+  updatedAt: '2026-06-01T03:15:50Z'
+});
